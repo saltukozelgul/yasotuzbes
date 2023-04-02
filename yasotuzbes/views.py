58445fe918728e36a6610949503be93cb0ebb1f8
saltukozelgul <saltukozelgul@gmail.com>
2023-04-02 15:03:11 +0300

    first commit

---
+++ yasotuzbes/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render
+import datetime
+#import json response
+from django.http.response import JsonResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def result(request):
+    if request.method == "POST":
+        data = request.POST.get('birthdate')
+        # create date object
+        date = datetime.datetime.strptime(data, '%Y-%m-%d')
+        date_string = date.strftime('%d %B %Y') 
+
+        # replace January with Ocak
+        #  I know its terrible xd. I will fix it later
+        date_string = date_string.replace('January', 'Ocak')
+        date_string = date_string.replace('February', 'Şubat')
+        date_string = date_string.replace('March', 'Mart')
+        date_string = date_string.replace('April', 'Nisan')
+        date_string = date_string.replace('May', 'Mayıs')
+        date_string = date_string.replace('June', 'Haziran')
+        date_string = date_string.replace('July', 'Temmuz')
+        date_string = date_string.replace('August', 'Ağustos')
+        date_string = date_string.replace('September', 'Eylül')
+        date_string = date_string.replace('October', 'Ekim')
+        date_string = date_string.replace('November', 'Kasım')
+        date_string = date_string.replace('December', 'Aralık')
+
+        ## calculate the age
+        age = datetime.datetime.now().year - date.year
+
+        ## If the age is 35 perctange is 50% and 
+        perc = age / 70 * 100;
+        ## perc to fixes 2
+        perc = round(perc, 2)
+
+        ## 0-25 25-50 50-75 75-100 for perc
+        if perc >= 0 and perc <= 25:
+            quote = "Zamanla nasıl değişiyor insan hangi resmime baksam ben değilim!"
+        elif perc >= 25 and perc <= 50:
+            quote = "Gökyüzünün başka rengi de varmış! \n Geç fark ettim taşın sert olduğunu."
+        elif perc >= 50 and perc <= 75:
+            quote = "Yaş 35 yolun yarısı eder. \n Dante gibi ortasındayız ömrün." 
+        else:
+            quote = "Ne dönüp duruyor havada kuşlar?\n Nerden çıktı bu cenaze? Ölen kim?"
+        if age >= 70:
+            return render(request, 'dead.html')
+        return render(request, 'result.html', {'age': age, 'perc': perc, 'birthdate': date_string, 'quote': quote})
+
+
+    
